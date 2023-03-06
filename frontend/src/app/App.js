e127ea9cb6409c9fc5a2fdab6e551ff2bf91121f
shycoldii <alexandrova-2001@mail.ru>
2023-03-06 20:18:58 +0300

    Стартовый UI

---
+++ frontend/src/app/App.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Route, Switch, withRouter} from 'react-router-dom';
+
+import {getCurrentUser} from '../util/APIUtils';
+import {ACCESS_TOKEN} from '../constants';
+
+import Login from '../user/login/Login';
+import Signup from '../user/signup/Signup';
+import Profile from '../user/profile/Profile';
+import AppHeader from '../common/AppHeader';
+import LoadingIndicator from '../common/LoadingIndicator';
+
+import {Layout, notification} from 'antd';
+
+const {Content} = Layout;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: null,
+            isAuthenticated: false,
+            isLoading: true
+        }
+        this.handleLogout = this.handleLogout.bind(this);
+        this.loadCurrentUser = this.loadCurrentUser.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+
+        notification.config({
+            placement: 'topRight',
+            top: 70,
+            duration: 3,
+        });
+    }
+
+    loadCurrentUser() {
+        getCurrentUser()
+            .then(response => {
+                this.setState({
+                    currentUser: response,
+                    isAuthenticated: true,
+                    isLoading: false
+                });
+            }).catch(error => {
+            this.setState({
+                isLoading: false
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.loadCurrentUser();
+    }
+
+    handleLogout(redirectTo = "/", notificationType = "success", description = "You're successfully logged out.") {
+        localStorage.removeItem(ACCESS_TOKEN);
+
+        this.setState({
+            currentUser: null,
+            isAuthenticated: false
+        });
+
+        this.props.history.push(redirectTo);
+
+        notification[notificationType]({
+            message: 'resume me',
+            description: description,
+        });
+    }
+
+    handleLogin() {
+        notification.success({
+            message: 'resume me',
+            description: "You're successfully logged in.",
+        });
+        this.loadCurrentUser();
+        this.props.history.push("/");
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return <LoadingIndicator/>
+        }
+
+        return (
+            <Layout className="app-container">
+                <AppHeader isAuthenticated={this.state.isAuthenticated}
+                           currentUser={this.state.currentUser}
+                           onLogout={this.handleLogout}/>
+
+                <Content className="app-content">
+                    <div className="container">
+                        <Switch>
+                            <Route path="/login"
+                                   render={(props) => <Login onLogin={this.handleLogin} {...props} />}/>
+                            <Route path="/signup" component={Signup}/>
+                            <Route path="/users/:username"
+                                   render={(props) => <Profile isAuthenticated={this.state.isAuthenticated}
+                                                               currentUser={this.state.currentUser} {...props}  />}>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Content>
+            </Layout>
+        );
+    }
+}
+
+export default withRouter(App);
